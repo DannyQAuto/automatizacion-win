d29c822936aa6aee279d869555963b3c523da9e4
Paul Torres Munares <ptorres.qsystem@win.pe>
2025-11-20 17:29:22 -0500

    Suspension parte 3

--- test/pagesapp/app-suspension-page.ts
+++ test/pagesapp/app-suspension-page.ts
@@ -11,7 +11,7 @@ export class SuspensionPage extends PageBase {
     public readonly comboTipoSolicitud = '-android uiautomator:new UiSelector().descriptionContains("Tipo de solicitud")';
     public readonly SeleccionarSuspension = '-android uiautomator:new UiSelector().description("Suspensión Temporal")';
     public readonly SeleccionarFechaIni = '-android uiautomator:new UiSelector().descriptionContains("Inicio de la suspensión")';
-    public readonly SeleccionarDiaIni = '-android uiautomator:new UiSelector().description("19, Wednesday, November 19, 2025")';
+    public readonly SeleccionarDiaIni = '-android uiautomator:new UiSelector().description("21, Friday, November 21, 2025")';
     public readonly SeleccionarFlecha = '-android uiautomator:new UiSelector().className("android.widget.Button").instance(3)';
     public readonly SeleccionarFechaFin = '-android uiautomator:new UiSelector().description("9, Tuesday, December 9, 2025")';
     public readonly SeleccionarBtnListo = '-android uiautomator:new UiSelector().description("Listo")';
@@ -20,7 +20,17 @@ export class SuspensionPage extends PageBase {
     public readonly CheckBox = '-android uiautomator:new UiSelector().className("android.widget.CheckBox")';
     public readonly SeleccionarBtnEnviar = '-android uiautomator:new UiSelector().description("Enviar")';
     public readonly SeleccionarBtnConfirmar = '-android uiautomator:new UiSelector().description("Confirmar")';
-    public readonly ObtenerCodigoPedido = '-android uiautomator:new UiSelector().description("Código de pedido")';
+    //public readonly ObtenerCodigoPedido = '-android uiautomator:new UiSelector().description("Código de pedido")';
+    //public readonly ObtenerCodigoPedido = '//android.view.View[@content-desc="Código de pedido"]/following-sibling::*[1]';
+    public readonly ObtenerCodigoPedido = '-android uiautomator:new UiSelector().descriptionMatches("^[0-9]+$")';
+    public readonly obtenerCodigoSolicitud = '-android uiautomator:new UiSelector().descriptionContains("WIN-RQ-")';
+    public readonly ObtenerEstado = '//android.view.View[@content-desc="Estado"]/following-sibling::*[1]';
+    public readonly ObtenerFechaInicio = '//android.view.View[@content-desc="Inicio de suspensión temporal"]/following-sibling::*[1]';
+    public readonly ObtenerFechaFin = '//android.view.View[@content-desc="Fin de suspensión temporal"]/following-sibling::*[1]';
+    public readonly ObtenerDiasSuspen = '//android.view.View[@content-desc="Días suspendidos"]/following-sibling::*[1]';
+    public readonly ObtenerDiasDisponi = '//android.view.View[@content-desc="Días disponibles"]/following-sibling::*[1]';
+
+
 
 
     async IngresarSuspension(celular: string): Promise<void> {
@@ -59,11 +69,54 @@ export class SuspensionPage extends PageBase {
             await this.click(this.SeleccionarBtnEnviar);
             await browser.pause(1000);
             await this.click(this.SeleccionarBtnConfirmar);
-            await browser.pause(1000);
-            await this.click(this.ObtenerCodigoPedido);
-            const codigo = await $(this.ObtenerCodigoPedido).getText();
-            console.log('📦 CÓDIGO DE PEDIDO:', codigo);
             await browser.pause(10000);
+            //await this.click(this.ObtenerCodigoPedido);
+            //const codigo = await $(this.ObtenerCodigoPedido).getText();
+            //console.log('📦 CÓDIGO DE PEDIDO:', codigo);
+            //const codigo = await $(this.ObtenerCodigoPedido).getText();
+            const solicitud = await $(this.obtenerCodigoSolicitud).getAttribute('content-desc');
+            console.log("📦 CÓDIGO DE SOLICITUD:", solicitud);
+            expect(solicitud).toBeDefined();
+            expect(solicitud).not.toBe('');
+            expect(solicitud).toMatch(/^WIN-RQ-\d{4}-\d{5}$/);
+            console.log('✅ Validación exitosa: Código de Solicitud es válido:', solicitud);
+            await browser.pause(1000);
+            const codigo = await $(this.ObtenerCodigoPedido).getAttribute('content-desc');
+            console.log("📦 CÓDIGO DE PEDIDO:", codigo);
+            expect(codigo).toBeDefined();
+            expect(codigo).not.toBe('');
+            console.log('✅ Validación exitosa: Código de Pedido es válido:', codigo);
+            await browser.pause(1000);
+            const estado = await $(this.ObtenerEstado).getAttribute('content-desc');
+            console.log("📦 ESTADO:", estado);
+            expect(estado).toBeDefined();
+            expect(estado).not.toBe('');
+            console.log('✅ Validación exitosa: Estado es válido:', estado);
+            await browser.pause(1000);
+            const fechaini = await $(this.ObtenerFechaInicio).getAttribute('content-desc');
+            console.log("📦 FECHA DE INICIO:", fechaini);
+            expect(fechaini).toBeDefined();
+            expect(fechaini).not.toBe('');
+            console.log('✅ Validación exitosa: Fecha de Inicio es válido:', fechaini);
+            await browser.pause(1000);
+            const fechafin = await $(this.ObtenerFechaFin).getAttribute('content-desc');
+            console.log("📦 FECHA FIN:", fechafin);
+            expect(fechafin).toBeDefined();
+            expect(fechafin).not.toBe('');
+            console.log('✅ Validación exitosa: Fecha Fin es válido:', fechafin);
+            await browser.pause(1000);
+            const diassuspendidos = await $(this.ObtenerDiasSuspen).getAttribute('content-desc');
+            console.log("📦 CANTIDAD DE DÍAS SUSPENDIDOS:", diassuspendidos);
+            expect(diassuspendidos).toBeDefined();
+            expect(diassuspendidos).not.toBe('');
+            console.log('✅ Validación exitosa: Cantidad de Días suspendidos es válido:', diassuspendidos);
+            await browser.pause(1000);
+            const diasdisponibles = await $(this.ObtenerDiasDisponi).getAttribute('content-desc');
+            console.log("📦 CANTIDAD DE DÍAS DISPONIBLES:", diasdisponibles);
+            expect(diasdisponibles).toBeDefined();
+            expect(diasdisponibles).not.toBe('');
+            console.log('✅ Validación exitosa: Cantidad de Días disponibles es válido:', diasdisponibles);
+            await browser.pause(1000);
             //await this.waitForElement(this.AgregarSolicitud);
             //console.log('✅ En página de solicitudes');
         }
